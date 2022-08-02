3e7eacf582e271aaf218e0f1d30517f0eef028d5
vaysau <pamirstaspastas@gmail.com>
2022-08-03 00:04:51 +0300

    created models Client and Money

--- first_bank/bank/models.py
+++ first_bank/bank/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length=31)
+    balance = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+
+
+class Money(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    transaction = models.CharField(max_length=7)
+    trans_date = models.DateTimeField('transaction date', auto_now=True)
