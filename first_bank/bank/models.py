fcf54b971a72c6fb7eef91359ba443054f97f35b
vaysau <pamirstaspastas@gmail.com>
2022-08-08 22:22:19 +0300

    edited models

--- first_bank/bank/models.py
+++ first_bank/bank/models.py
@@ -13,13 +13,15 @@ class Money(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     trans_date = models.DateField('transaction date', default=date.today)
+    interest_amount = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    balance = models.DecimalField(max_digits=8, decimal_places=2, default=0)
 
     def __str__(self):
-        return self.amount, self.trans_date
+        return self.balance
 
 
 class Interest(models.Model):
-    interest = models.DecimalField(max_digits=3, decimal_places=1, default=0)
+    interest_rate = models.DecimalField(max_digits=3, decimal_places=1, default=0)
 
     def __str__(self):
         return self.interest
